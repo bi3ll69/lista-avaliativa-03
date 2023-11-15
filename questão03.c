5e87ce3b58706e3bde0d1e90086b7c4047a0e828
bi3ll69 <bielneres7@gmail.com>
2023-11-14 21:00:00 -0300

    aaaaaaaaaaaaaaaaaaaa

--- questão03.c
+++ questão03.c
@@ -3,72 +3,64 @@
 #include <ctype.h>
 
 int main() {
-    char placa[10], dia[20];
-    
-    scanf("%s", placa);
-    scanf("%s", dia);
+    char placa[10];
+    char dia_semana[20];
 
-    if (strlen(placa) == 7 || strlen(placa) == 8) {
-        int i;
-        for (i = 0; i < 3; ++i) {
-            if (!isalpha(placa[i])) {
-                printf("Placa invalida\n");
-                return 0;
-            }
-        }
-        if (placa[3] != '-') {
-            printf("Placa invalida\n");
-            return 0;
-        }
-        for (i = 4; i < strlen(placa); ++i) {
-            if (!isdigit(placa[i])) {
-                printf("Placa invalida\n");
-                return 0;
-            }
-        }
-    } else {
+    // Leitura da entrada
+    scanf("%s", placa);
+    scanf("%s", dia_semana);
+
+    // Verificação do formato da placa
+    if ((strlen(placa) != 7 && strlen(placa) != 8) || (placa[3] != '-' && placa[3] != 'N')) {
         printf("Placa invalida\n");
         return 0;
     }
 
-    for (int i = 0; i < strlen(dia); ++i) {
-        dia[i] = toupper(dia[i]);
+    // Verificação do formato do dia da semana
+    for (int i = 0; i < strlen(dia_semana); i++) {
+        dia_semana[i] = toupper(dia_semana[i]);
     }
 
-    if (strcmp(dia, "SEGUNDA-FEIRA") == 0) {
-        if (placa[6] == '0' || placa[6] == '1') {
-            printf("%s nao pode circular segunda-feira\n", placa);
-        } else {
-            printf("%s pode circular segunda-feira\n", placa);
-        }
-    } else if (strcmp(dia, "TERCA-FEIRA") == 0) {
-        if (placa[6] == '2' || placa[6] == '3') {
-            printf("%s nao pode circular terca-feira\n", placa);
-        } else {
-            printf("%s pode circular terca-feira\n", placa);
-        }
-    } else if (strcmp(dia, "QUARTA-FEIRA") == 0) {
-        if (placa[6] == '4' || placa[6] == '5') {
-            printf("%s nao pode circular quarta-feira\n", placa);
-        } else {
-            printf("%s pode circular quarta-feira\n", placa);
-        }
-    } else if (strcmp(dia, "QUINTA-FEIRA") == 0) {
-        if (placa[6] == '6' || placa[6] == '7') {
-            printf("%s nao pode circular quinta-feira\n", placa);
-        } else {
-            printf("%s pode circular quinta-feira\n", placa);
-        }
-    } else if (strcmp(dia, "SEXTA-FEIRA") == 0) {
-        if (placa[6] == '8' || placa[6] == '9') {
-            printf("%s nao pode circular sexta-feira\n", placa);
-        } else {
-            printf("%s pode circular sexta-feira\n", placa);
-        }
-    } else if (strcmp(dia, "SABADO") == 0 || strcmp(dia, "DOMINGO") == 0) {
-        printf("Nao ha proibicao no fim de semana\n");
-    } else {
+    if (strcmp(dia_semana, "SEGUNDA-FEIRA") != 0 && strcmp(dia_semana, "TERCA-FEIRA") != 0 &&
+        strcmp(dia_semana, "QUARTA-FEIRA") != 0 && strcmp(dia_semana, "QUINTA-FEIRA") != 0 &&
+        strcmp(dia_semana, "SEXTA-FEIRA") != 0 && strcmp(dia_semana, "SABADO") != 0 &&
+        strcmp(dia_semana, "DOMINGO") != 0) {
         printf("Dia da semana invalido\n");
+        return 0;
+    }
+
+    // Verificação do rodízio
+    char ult_digito = placa[strlen(placa) - 1];
+    int proibido = 0;
+
+    switch (dia_semana[0]) {
+        case 'S':
+            printf("Nao ha proibicao no fim de semana\n");
+            break;
+        case 'Q':
+            if (ult_digito == '0' || ult_digito == '1') proibido = 1;
+            break;
+        case 'T':
+            if (ult_digito == '2' || ult_digito == '3') proibido = 1;
+            break;
+        case 'Q':
+            if (ult_digito == '4' || ult_digito == '5') proibido = 1;
+            break;
+        case 'S':
+            if (ult_digito == '6' || ult_digito == '7') proibido = 1;
+            break;
+        case 'S':
+            if (ult_digito == '8' || ult_digito == '9') proibido = 1;
+            break;
+        default:
+            printf("Dia da semana invalido\n");
+            return 0;
+    }
+
+    if (proibido) {
+        printf("%s nao pode circular %s\n", placa, dia_semana);
+    } else {
+        printf("%s pode circular %s\n", placa, dia_semana);
     }
 
     return 0;
